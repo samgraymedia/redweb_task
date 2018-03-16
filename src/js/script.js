b4e5385aab65d7ca817230abdad625af338a36ea
Samuel Gray <sam.gray22@gmail.com>
2018-03-16 22:18:54 +0000

    ajax

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,11 @@
 $.ajax({
-  url: "https://jsonplaceholder.typicode.com/posts/",
+  url: "https://jsonplaceholder.typicode.com/users",
   cache: false,
-  success: function() {
-    console.log("done");
+  success: function(data) {
+    console.log(data);
+    $(data).each(function() {
+      $('.data').append('<h1>' + this.name + '</h1>')
+    });
   },
   complete: function() {
     $('#loading-image').hide();
