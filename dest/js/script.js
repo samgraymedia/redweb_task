54d5feca818a4264680c61dd5f8f5923953b8ea7
Samuel Gray <sam.gray22@gmail.com>
2018-03-27 18:29:53 +0100

    Dymanically pulling dropdown content

--- dest/js/script.js
+++ dest/js/script.js
@@ -1,10 +1,10 @@
 $.ajax({
-  url: "https://jsonplaceholder.typicode.com/users",
+  url: "../data/book-data.json",
   cache: false,
   success: function(data) {
     console.log(data);
-    $(data).each(function() {
-      $('.data').append('<h1>' + this.name + '</h1>' + '<p>' + this.username + '</p>')
+    $(data.books).each(function() {
+      $('.dropdown').append('<option>' + this.title + '</option>')
     });
   },
   complete: function() {
